c0c437a99016230ee7a36946e248df9d9677488f
Duleendra <shashimald@gmail.com>
2022-10-19 11:37:02 +0800

    Update index.js
    Added new char

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function handler(event, context, callback) {
-    let text = "Count how many characters";
+    let text = "Shashimal Count how many characters";
     let length = text.length;
 
     callback(null, {
